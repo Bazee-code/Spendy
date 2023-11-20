eaa713bc8cccefae98ab611df800d7f0915e1b12
Bazee-code <eugeneobazee2@gmail.com>
2023-11-20 17:17:34 +0300

    Refactor styles into separate stylesheet

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -20,26 +20,10 @@ const Home = () => {
               Total Spent
             </Text>
           </View>
-          <Text
-            style={{
-              color: '#FFF',
-              fontSize: 33,
-              fontWeight: '800',
-              marginTop: 10,
-            }}
-          >
-            6,000 KES
-          </Text>
+          <Text style={styles.amountTitle}>6,000 KES</Text>
         </View>
         <View style={styles.descriptionContainer}>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginTop: 10,
-            }}
-          >
+          <View style={styles.descriptionTextContainer}>
             <Text style={{ fontSize: 22, color: '#080416' }}>Payments</Text>
             <Text style={{ fontSize: 16, color: '#808080', fontWeight: '600' }}>
               View all
@@ -54,81 +38,27 @@ const Home = () => {
               payments.map((payment, index) => (
                 <View key={payment.id} style={styles.paymentContainer}>
                   <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                    <View
-                      style={{
-                        height: 20,
-                        width: 20,
-                        borderRadius: 10,
-                        backgroundColor: payment.color,
-                        marginRight: 10,
-                      }}
-                    />
+                    <View style={styles.dot} />
                     <View>
-                      <Text
-                        style={{
-                          color: '#080416',
-                          fontSize: 22,
-                          fontWeight: '600',
-                        }}
-                      >
-                        {payment.title}
-                      </Text>
-                      <Text
-                        style={{
-                          color: '#808080',
-                          fontSize: 15,
-                          fontWeight: '600',
-                        }}
-                      >
+                      <Text style={styles.paymentTitle}>{payment.title}</Text>
+                      <Text style={styles.paymentCategory}>
                         {payment.categories}
                       </Text>
                     </View>
                   </View>
                   <View>
                     {payment.reminderSet ? (
-                      <Text
-                        style={{
-                          color: '#808080',
-                          fontSize: 18,
-                          fontWeight: '600',
-                        }}
-                      >
-                        Reminder set
-                      </Text>
+                      <Text style={styles.reminder}>Reminder set</Text>
                     ) : (
-                      <Text
-                        style={{
-                          color: '#808080',
-                          fontSize: 18,
-                          fontWeight: '600',
-                        }}
-                      >
-                        No reminder
-                      </Text>
+                      <Text style={styles.reminder}>No reminder</Text>
                     )}
-                    <Text
-                      style={{
-                        color: '#808080',
-                        fontSize: 14,
-                        fontWeight: '600',
-                      }}
-                    >
-                      {payment.date}
-                    </Text>
+                    <Text style={styles.date}>{payment.date}</Text>
                   </View>
                 </View>
               ))
             ) : (
               <View style={{ height: windowHeight * 0.6 }}>
-                <Text
-                  style={{
-                    color: '#808080',
-                    fontSize: 22,
-                    fontWeight: '600',
-                  }}
-                >
-                  No payments made
-                </Text>
+                <Text style={styles.noPayment}>No payments made</Text>
               </View>
             )}
             <View style={{ height: windowHeight * 0.5 }} />
@@ -136,20 +66,7 @@ const Home = () => {
         </View>
       </View>
 
-      <TouchableOpacity
-        style={{
-          position: 'absolute',
-          bottom: 5,
-          right: 10,
-          backgroundColor: '#6C63FF',
-          height: 50,
-          width: 50,
-          borderRadius: 25,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
+      <TouchableOpacity style={styles.addButton}>
         <Octicons name="plus" size={24} color="#FFF" />
       </TouchableOpacity>
     </>
