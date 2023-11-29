0f1c88520671da31f5d99a8cea784fc4769879b0
Bazee-code <eugeneobazee2@gmail.com>
2023-11-29 17:35:29 +0300

    Add profile screen ui, clear up unused tabs

--- src/screens/profile/index.js
+++ src/screens/profile/index.js
@@ -1,16 +1,78 @@
-import { View, Text, SafeAreaView } from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import React from 'react';
 import { styles } from './styles';
+import { Feather, MaterialIcons, AntDesign } from '@expo/vector-icons';
 
 const ProfileScreen = () => {
   return (
     <View>
       <SafeAreaView style={{ flex: 1 }} />
-      <Text style={{ fontSize: 22, color: '#16112B', textAlign: 'center' }}>
-        Profile
-      </Text>
-      <View>
-        
+      <View style={styles.container}>
+        <Text style={styles.title}>Profile</Text>
+        <View style={styles.profileContainer}>
+          <Image
+            source={require('../../../assets/profile.jpg')}
+            style={styles.profileImage}
+          />
+          <TouchableOpacity style={styles.editButton}>
+            <Feather name="edit-2" size={18} color="#FFF" />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={styles.profileName}>Eugene Obare</Text>
+        </View>
+        <View style={styles.formContainer}>
+          <View style={styles.icon}>
+            <MaterialIcons name="email" size={18} color="#FFF" />
+          </View>
+          <View>
+            <Text style={styles.inputTitle}>Email</Text>
+            <TextInput
+              value="eugeneobare@gmail.com"
+              style={styles.input}
+              disabled={true}
+            />
+          </View>
+        </View>
+        <View style={[styles.formContainer, { marginVertical: 20 }]}>
+          <View style={styles.icon}>
+            <AntDesign name="calendar" size={18} color="#FFF" />
+          </View>
+          <View>
+            <Text style={styles.inputTitle}>Registered in</Text>
+            <TextInput
+              value="29 Nov 2023"
+              style={styles.input}
+              disabled={true}
+            />
+          </View>
+        </View>
+        <View style={styles.formContainer}>
+          <View style={styles.icon}>
+            <AntDesign name="user" size={18} color="#FFF" />
+          </View>
+          <View>
+            <Text style={styles.inputTitle}>You invite, you win</Text>
+          </View>
+        </View>
+        <View style={[styles.formContainer, { marginTop: 50 }]}>
+          <MaterialIcons name="logout" size={30} color="#6C63FF" />
+          <View>
+            <Text style={[styles.inputTitle, { marginLeft: 10, fontSize: 20 }]}>
+              Logout
+            </Text>
+          </View>
+        </View>
+        <Text style={[styles.title, { marginVertical: 40, fontSize: 18 }]}>
+          Version 1.0.0
+        </Text>
       </View>
     </View>
   );
--- src/screens/profile/styles.js
+++ src/screens/profile/styles.js
@@ -1,3 +1,74 @@
 import { StyleSheet } from 'react-native';
+import { windowHeight, windowWidth } from '../../configs/dimensions';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    height: windowHeight,
+  },
+  title: {
+    fontSize: 22,
+    color: '#16112B',
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  profileImage: {
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+  },
+  profileName: {
+    fontSize: 20,
+    color: '#16112B',
+    fontWeight: '600',
+    marginVertical: 10,
+    textAlign: 'center',
+  },
+  editButton: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    backgroundColor: '#6C63FF',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 3,
+    right: 150,
+  },
+  formContainer: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    height: 30,
+    width: 30,
+    borderRadius: 15,
+    backgroundColor: '#6C63FF',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 10,
+  },
+  input: {
+    borderTopColor: '#FFF',
+    borderLeftColor: '#FFF',
+    borderRightColor: '#FFF',
+    borderBottomColor: '#16112B',
+    borderBottomWidth: 1,
+    width: windowWidth * 0.85,
+    paddingBottom: 5,
+    fontSize: 18,
+    color: '#16112B',
+  },
+  inputTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#16112B',
+  },
+});
