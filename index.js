e0c4140ba67176f736023c80eb144090d2126218
Bazee-code <eugeneobazee2@gmail.com>
2023-12-08 16:16:42 +0300

    Install & setup @apollo/client & graphql

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import 'react-native-gesture-handler';
 
-import { View, Text } from 'react-native';
 import React from 'react';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const index = () => {
-  return <App />;
+  return;
+  <ApolloProvider client={client}>
+    <App />;
+  </ApolloProvider>;
 };
 
 export default index;
